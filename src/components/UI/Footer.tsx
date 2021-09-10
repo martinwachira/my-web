acfbb7b7f205871d0074382e91b2d7f072afab4e
martinwachira <martinnwachira@gmail.com>
2021-09-11 01:09:21 +0300

    update: make changes on the timer value

--- src/components/UI/Footer.tsx
+++ src/components/UI/Footer.tsx
@@ -1,14 +1,28 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+
 import classes from "../Assets/universal.module.css";
 
 const Footer: React.FC = () => {
-  let date_today = new Date().toLocaleString();
+  //   let date_today = new Date().toLocaleString();
+  //   const [curTime, setCurTime] = useState("");
+  const [date, setDate] = useState(new Date());
+
+  useEffect(() => {
+    let timerID = setInterval(() => tick(), 1000);
+    return function cleanup() {
+      clearInterval(timerID);
+    };
+  });
+
+  const tick = () => {
+    setDate(new Date());
+  };
   return (
     <h5>
       <span style={{ color: "springgreen" }}>console.log</span>
       <span className={classes.obj_color}>(</span>'copyright ©{" "}
       <span className={classes.copy_name}>WCR: </span>{" "}
-      <span className={classes.date_color}>{date_today}</span>'{" "}
+      <span className={classes.date_color}>{date.toLocaleString()}</span>'{" "}
       <span className={classes.obj_color}>)</span>
     </h5>
   );

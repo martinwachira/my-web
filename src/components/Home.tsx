6bab8fbf4241eba1b6671e59604e564eb108a348
martinwachira <martinnwachira@gmail.com>
2021-09-10 23:18:13 +0300

    add: init new svg comp

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,7 +1,12 @@
 import React from "react";
+import Svg from "./UI/Svg";
 
 const Home: React.FC = () => {
-  return <h2>Home</h2>;
+  return (
+    <h2>
+      <Svg />
+    </h2>
+  );
 };
 
 export default Home;

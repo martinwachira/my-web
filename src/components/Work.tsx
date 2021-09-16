cf61244fdea39562a3aee591e86d31d2e2743187
martinwachira <martinnwachira@gmail.com>
2021-09-16 16:38:35 +0300

    init: work content

--- src/components/Work.tsx
+++ src/components/Work.tsx
@@ -1,7 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
+
+import classes from "./Assets/universal.module.css";
 
 const Work: React.FC = () => {
-  return <h2>Work</h2>;
+  const [startDate, setStartDate] = useState();
+  return (
+    <>
+      <div className={classes["about-content"]}>
+        <h4>selected_work()</h4>
+        <div style={{ color: "gray" }}>
+          <h5>Safaricom - Product Development Engineer - </h5>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Work;

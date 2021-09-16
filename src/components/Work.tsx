66992cf90e985b1ab68de129bbf6330796d5f3cc
martinwachira <martinnwachira@gmail.com>
2021-09-16 19:22:59 +0300

    update: work dates

--- src/components/Work.tsx
+++ src/components/Work.tsx
@@ -1,15 +1,45 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 import classes from "./Assets/universal.module.css";
 
+interface JobDone {
+  currentYr: Date;
+}
+
 const Work: React.FC = () => {
-  const [startDate, setStartDate] = useState();
+  const [startDate, setStartDate] = useState<JobDone>({
+    currentYr: new Date(),
+  });
+  useEffect(() => {
+    setStartDate((prevState) => {
+      return {
+        ...prevState,
+        currentYr: new Date(),
+      };
+    });
+  }, []);
+
+  const safStart = new Date("08/01/2021");
+  const yrsSaf = startDate.currentYr.getFullYear() - safStart.getFullYear();
+  const mnthsSaf = startDate.currentYr.getMonth() - safStart.getMonth() + 1;
+
+  console.log("years saf", yrsSaf);
+  console.log("months saf", mnthsSaf);
+
+  console.log();
   return (
     <>
       <div className={classes["about-content"]}>
-        <h4>selected_work()</h4>
+        <h3>selected_work()</h3>
         <div style={{ color: "gray" }}>
-          <h5>Safaricom - Product Development Engineer - </h5>
+          <h4>Product Development Engineer</h4>
+          <span>
+            Safaricom PLC .{" "}
+            {safStart.toLocaleString("default", { month: "short" })}{" "}
+            {safStart.getFullYear()} - {yrsSaf}
+            yrs and {mnthsSaf}
+            months{" "}
+          </span>
         </div>
       </div>
     </>

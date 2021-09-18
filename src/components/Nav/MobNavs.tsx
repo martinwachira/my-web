745eab1b753d5a981b0af8f6a95af349b7dd304c
martinwachira <martinnwachira@gmail.com>
2021-09-18 16:56:04 +0300

    update: handle hamburger menu

--- src/components/Nav/MobNavs.tsx
+++ src/components/Nav/MobNavs.tsx
@@ -1,14 +1,27 @@
 // import { FaLinkedinIn, FaStackOverflow, FaTwitter } from "react-icons/fa";
 
+import React, { useState } from "react";
+
 import NavLinks from "./NavLinks";
-import React from "react";
+import { TiThMenu } from "react-icons/ti";
 import classes from "./Navs.module.css";
 
 const MobNavs: React.FC = () => {
+  const [openMenu, setOpenMenu] = useState(false);
+
+  const onMenuHandler = () => {
+    setOpenMenu(!openMenu);
+  };
   return (
     <>
       <nav className={classes["mob-navs"]}>
-        <NavLinks />
+        <TiThMenu
+          className={classes.hamburger}
+          size="40px"
+          color="springgreen"
+          onClick={onMenuHandler}
+        />
+        {openMenu && <NavLinks />}
       </nav>
     </>
   );

253dbd36494f1ef7f37f740c72951aa5ef5944f7
martinwachira <martinnwachira@gmail.com>
2021-09-18 12:09:21 +0300

    update: init new navs comp

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,13 @@
 import "./App.css";
 
-import { FaLinkedinIn, FaStackOverflow, FaTwitter } from "react-icons/fa";
 import { Link, Route, BrowserRouter as Router, Switch } from "react-router-dom";
 
 import About from "./components/About";
-// import Blinker from "./components/UI/Blinker";
 import Contact from "./components/Contact";
 import Footer from "./components/UI/Footer";
 import Home from "./components/Home";
 import MyImg from "./components/UI/MyImg";
+import Navs from "./components/UI/Navs";
 import React from "react";
 import Work from "./components/Work";
 
@@ -24,31 +23,14 @@ const App: React.FC = () => {
               <Link to="/">127.0.0.1</Link>
             </nav>
             <nav className="other-navs">
-              <Link to="/about">?.about</Link>
-              <Link to="/work">work()</Link>
-              <Link to="/contact">_contact['']</Link>
-              <a href="https://www.linkedin.com/in/martin-wachira-82a243117/">
-                <FaLinkedinIn />
-              </a>
-              <a href="https://twitter.com/martinwcr">
-                <FaTwitter />
-              </a>
-              <a href="https://stackoverflow.com/users/13235540/martin-wcr">
-                <FaStackOverflow />
-              </a>
+              <Navs />
             </nav>
           </div>
-          {/* <div className="blinker">
-            <Blinker />
-          </div> */}
 
           {/* section body */}
           <Switch>
             <Route path="/about">
-              <About
-              // name="Martin  Wachira"
-              // occupation="Web Software Developer"
-              />
+              <About />
             </Route>
             <Route path="/work">
               <Work />

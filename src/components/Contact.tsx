eb1811880f21ada0b1e5b1ef75b30f88af01be97
martinwachira <martinnwachira@gmail.com>
2021-09-27 22:16:57 +0300

    fix conatct bug

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,7 +1,70 @@
-import React from "react";
+import React, { useRef } from "react";
+
+import classes from "./Assets/contact.module.css";
+import { postRemarks } from "../api/ContactsApi";
+import uniClasses from "./Assets/universal.module.css";
+
+// import TextField from "@mui/material/TextField";
 
 const Contact: React.FC = () => {
-  return <h2>Contact</h2>;
+  const nameRef = useRef<HTMLInputElement>(null);
+  const addressRef = useRef<HTMLInputElement>(null);
+  const remarksRef = useRef<HTMLTextAreaElement>(null);
+
+  const submitHandler = (e: any) => {
+    e.preventDefault();
+
+    const remarks = {
+      name: nameRef.current,
+      address: addressRef.current,
+      remarks: remarksRef.current,
+    };
+
+    postRemarks(remarks);
+  };
+
+  console.log("name ref", nameRef);
+
+  return (
+    <form onSubmit={submitHandler}>
+      <div className={classes.control}>
+        <div className={uniClasses["about-content"]}>
+          <h2>Contact</h2>
+          <label htmlFor="name">Name</label>
+          <input type="text" id="name" ref={nameRef} />
+        </div>
+        <div className={classes.control}>
+          <label htmlFor="email">Email Address</label>
+          <input type="text" id="email" ref={addressRef} />
+        </div>
+        <div className={classes.control}>
+          <label htmlFor="remarks">Remarks</label>
+          <textarea id="remarks" ref={remarksRef}></textarea>
+        </div>
+        {/* <div className={classes.control}>
+        <label htmlFor="date">Release Date</label>
+        <input type="text" id="date" ref={releaseDateRef} />
+      </div> */}
+        <button>Send your Remarks</button>
+        {/* <TextField
+        style={{ color: "white" }}
+        id="outlined-basic"
+        label="Outlined"
+        variant="outlined"
+      />
+      <TextField id="filled-basic" label="Filled" variant="filled" />
+      <TextField id="standard-basic" label="Standard" variant="standard" />
+      <TextField
+        id="outlined-read-only-input"
+        label="Read Only"
+        defaultValue="Hello World"
+        InputProps={{
+          readOnly: true,
+        }}
+      /> */}
+      </div>
+    </form>
+  );
 };
 
 export default Contact;

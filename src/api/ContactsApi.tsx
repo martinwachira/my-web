be4855336707f2fee73c3a3dbf68303c3116add7
martinwachira <martinnwachira@gmail.com>
2021-09-28 00:00:26 +0300

    contact api send remarks

--- src/api/ContactsApi.tsx
+++ src/api/ContactsApi.tsx
@@ -1,17 +1,17 @@
 export async function postRemarks(remarks: any) {
-  // const res = await fetch(
-  //   "https://react-redefined-default-rtdb.firebaseio.com/movies.json",
-  //   {
-  //     method: "POST",
-  //     body: JSON.stringify(remarks),
-  //     headers: {
-  //       "Content-Type": "Application/json",
-  //     },
-  //   }
-  // );
-  // const data = await res.json();
-  // if (res.status === 200) {
-  //   console.log("data", data);
-  // }
-  console.log("posting data");
+  const res = await fetch(
+    "https://my-portfolio-d5c38-default-rtdb.firebaseio.com/remarks.json",
+    {
+      method: "POST",
+      body: JSON.stringify(remarks),
+      headers: {
+        "Content-Type": "Application/json",
+      },
+    }
+  );
+  const data = await res.json();
+  if (res.status === 200) {
+    console.log("data", data);
+  }
+  // console.log("posting data");
 }
